d00a31b2470451cf36d7e506a7412c73f97e7486
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-07-04 11:53:53 -0700

    Update Coloured-triangles.js

--- Coloured-triangles.js
+++ Coloured-triangles.js
@@ -43,3 +43,25 @@ const triangle = row => {
 }
 
 // or
+
+function triangle(row) {
+  return row.length == 1 ? row : triangle(processedRow(row))
+}
+
+function processedRow(row) {
+  return row.split('').map((el, i) => i == row.length-1 ? null : sumLetters(row[i], row[i+1])).join('');
+}
+
+function sumLetters(a, b) {
+  switch (a+b) {
+    case 'BG':
+    case 'GB':
+    case 'RR': return 'R'; break;
+    case 'BR':
+    case 'RB':
+    case 'GG': return 'G'; break;
+    case 'RG':
+    case 'GR':
+    case 'BB': return 'B'; break;
+  }
+}
